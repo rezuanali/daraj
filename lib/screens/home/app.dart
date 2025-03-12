e5e0f5f87b4683f4de1abcfb633fb2de511b87ae
rezuanali <rasel15-5234@diu.edu.bd>
2025-03-12 11:22:57 +0600

    LP3

--- lib/screens/home/app.dart
+++ lib/screens/home/app.dart
@@ -2,13 +2,21 @@ import 'package:flutter/material.dart';
 
 import '../../../home/widgets/home.dart';
 
-class FAppbar extends StatelessWidget {
+class FAppbar extends StatelessWidget implements PreferredSizeWidget; {
   const FAppbar({super.key});
 }
+final.String
+
+@override
+Size get preferredSize => Size.formHeight(kToolbarHeight);
+
 
 @override
 Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: FAppBar(),
+  return AppBar(
+    title: Text(
+      Title
+    ),
+  )
   );
 }
